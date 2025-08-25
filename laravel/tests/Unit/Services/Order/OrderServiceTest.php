f12f648c118a7d01c2f5c3e0f9641a6621cdc163
Ümit UZ <info@umituz.com>
2025-08-25 22:57:12 +0300

    Cleanup

--- laravel/tests/Unit/Services/Order/OrderServiceTest.php
+++ laravel/tests/Unit/Services/Order/OrderServiceTest.php
@@ -6,7 +6,6 @@ use App\DTOs\Order\OrderCreateDTO;
 use App\Enums\Order\OrderStatusEnum;
 use App\Exceptions\Order\EmptyCartException;
 use App\Exceptions\Product\InsufficientStockException;
-use App\Jobs\Order\SendOrderConfirmedJob;
 use App\Models\Cart\Cart;
 use App\Models\Cart\CartItem;
 use App\Models\Order\Order;
